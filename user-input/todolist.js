0a501332b2e8fa41aa610d74949ecd977dba310f
jongrover <hello@jonathangrover.com>
2015-08-11 17:32:43 -0400

    ad user-input lesson code

---
+++ user-input/todolist.js
@@ -0,0 +1,39 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+/// <reference path="typings/angular2/angular2.d.ts" />
+var angular2_1 = require('angular2/angular2');
+var ToDoList = (function () {
+    function ToDoList() {
+        this.todos = ["Eat Breakfast", "Walk Dog", "Breathe"];
+    }
+    ToDoList.prototype.addTodo = function (todo) {
+        this.todos.push(todo);
+    };
+    ToDoList.prototype.doneTyping = function ($event) {
+        if ($event.which === 13) {
+            this.addTodo($event.target.value);
+            $event.target.value = null;
+        }
+    };
+    ToDoList = __decorate([
+        angular2_1.Component({
+            selector: 'todo'
+        }),
+        angular2_1.View({
+            template: "\n    <ul>\n      <li *ng-for=\"#todo of todos\">\n        {{ todo }}\n      </li>\n    </ul>\n    <input #todotext>\n    <button (click)=\"addTodo(todotext.value)\">Add ToDo</button>\n  ",
+            directives: [angular2_1.NgFor]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ToDoList);
+    return ToDoList;
+})();
+angular2_1.bootstrap(ToDoList);
---
+++ user-input/todolist.ts
@@ -0,0 +1,39 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+
+@Component({
+  selector: 'todo'
+})
+@View({
+  template: 
+  `
+    <ul>
+      <li *ng-for="#todo of todos">
+        {{ todo }}
+      </li>
+    </ul>
+    <input #todotext>
+    <button (click)="addTodo(todotext.value)">Add ToDo</button>
+  `,
+  directives: [NgFor]
+})
+class ToDoList {
+  todos: Array<string>;
+
+  constructor() {
+    this.todos = ["Eat Breakfast", "Walk Dog", "Breathe"];
+  }
+
+  addTodo(todo: string) {
+    this.todos.push(todo);
+  }
+  
+  doneTyping($event) {
+    if($event.which === 13) {
+      this.addTodo($event.target.value);
+      $event.target.value = null;
+    }
+  }
+}
+
+bootstrap(ToDoList)
