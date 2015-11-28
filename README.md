0fe745be9f95a52947d06ef2cc83d917d96d3fe1
jongrover <hello@jonathangrover.com>
2015-11-27 23:43:33 -0500

    clean up and start updated app organization using npm and package.json, and tsconfig.json

--- README.md
+++ README.md
@@ -23,3 +23,26 @@ $ `tsc --watch -m commonjs -t es5 --emitDecoratorMetadata <your-file-to-watch>.t
 Run HTTP-Server:
 
 $ `http-server`
+
+## Updated Instructions Nov 2015
+
+Reccomended NPM 3.5.0 and Node v5.1.0 if you have lesser version it is reccomended you first install [NVM](https://github.com/creationix/nvm)
+
+Then install latest version of node using nvm:
+
+$ `nvm install node`
+
+Then install latest version of npm:
+
+$ `npm install -g npm`
+
+Create a package.json for new apps:
+
+$ `npm init -y`
+
+Add angular2 and typescript:
+
+$ `npm i angular2@2.0.0-alpha.44 systemjs@0.19.2 --save --save-exact`
+
+$ `npm i typescript live-server --save-dev`
+
--- inputs-outputs/src/components/app.ts
+++ inputs-outputs/src/components/app.ts
@@ -1,21 +1,34 @@
 ///<reference path="../../typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
-import {Foo} from './foo';
+import {Foo} from './foo.ts';
+
+class Person {
+  name: string;
+  constructor() {
+    this.name = "Bob";
+  }
+}
+
 
 @Component({
-  selector: 'app',
+  selector: 'app'
 })
 @View({
   directives: [Foo],
   template: `
     <p>{{ greet }}</p>
-    <foo></foo>
+    <input type="text" [value]="num">
+    <foo [person]="person"></foo>
    `
 })
 class App {
-  greet: string;
+  greet: string; // private input (property) available to only this component.
+  num: number;
+  person: Person; // public input (property)
   constructor(){
-    this.greet = "Hello"; //private input (property) available to only this component.
+    this.greet = "Hello";
+    this.num = 4;
+    this.person = new Person();
   }
 }
 
